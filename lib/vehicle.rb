76e0ca74a8fed3eca00c43da4f44d5c7ec78d1af
Christine Xiang <yutingcxiang@gmail.com>
2018-07-16 21:55:53 +0000

    automatically backed up by learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,11 @@
 class Vehicle
+  attr_accessor :wheel_size, :wheel_number
 
+  def initialize(wheel_size, wheel_number)
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
+  end
+  
+  def go
+    "vr"
 end
